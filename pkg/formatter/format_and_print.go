dd0e8776fd74047b396ad177bbacb771c1e838d7
grigorii.davydenko <grigorydavydenko@yandex.ru>
2024-05-01 15:04:19 +0300

    added tabular an csv writers

--- pkg/formatter/format_and_print.go
+++ pkg/formatter/format_and_print.go
@@ -1,14 +1,19 @@
 package formatter
 
 import (
+	"encoding/csv"
+	"fmt"
 	"github.com/DavydenkoGr/gitfame/pkg/author"
 	"github.com/DavydenkoGr/gitfame/pkg/sorter"
+	"os"
+	"strconv"
+	"text/tabwriter"
 )
 
 func (f *Formatter) FormatAndPrint(authorDict author.AuthorDict) {
 	authors := sorter.Authors{
 		SortKey: f.OrderBy,
-		Array: []*author.Author{},
+		Array:   []*author.Author{},
 	}
 
 	for _, author := range authorDict {
@@ -19,12 +24,52 @@ func (f *Formatter) FormatAndPrint(authorDict author.AuthorDict) {
 
 	switch f.Format {
 	case "tabular":
-		return
+		printTabular(authors)
 	case "csv":
-		return
+		printCSV(authors)
 	case "json":
 		return
 	case "json-lines":
 		return
 	}
+}
+
+func printTabular(authors sorter.Authors) {
+	tabwriter := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', 0)
+
+	fmt.Fprintln(tabwriter, "Name\tLines\tCommits\tFiles")
+
+	for _, author := range authors.Array {
+		fmt.Fprintf(
+			tabwriter,
+			"%s\t%d\t%d\t%d\n",
+			author.Name,
+			author.Lines,
+			author.Commits,
+			author.Files,
+		)
+	}
+
+	tabwriter.Flush()
+}
+
+func printCSV(authors sorter.Authors) {
+	records := [][]string{
+		{"Name", "Lines", "Commits", "Files"},
+	}
+
+	for _, author := range authors.Array {
+		records = append(
+			records,
+			[]string{
+				author.Name,
+				strconv.Itoa(author.Lines),
+				strconv.Itoa(author.Commits),
+				strconv.Itoa(author.Files),
+			},
+		)
+	}
+
+	w := csv.NewWriter(os.Stdout)
+	w.WriteAll(records)
 }
